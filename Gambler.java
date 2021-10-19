1954ffe011423c0cdd3ab000660bc5fb4dfe3a4f
Artimirche6 <artimirche6@gmail.com>
2021-10-19 18:49:56 +0530

    [arti]: Gambler if Win Lost 50% of the stake then Resign

--- Gambler.java
+++ Gambler.java
@@ -4,31 +4,59 @@ public class Gambler
 	{	
 		int amount = 100;
 		int betAmount = 1;
-		
+		int day, newAmount  = 0;
 		System.out.println("Gambler initial amount $" +amount);
 		System.out.println("Gambler can bet $" +betAmount+ " every game");
 		System.out.println();
-		System.out.println("Bet value is 1 then WON else LOSS");
 		
-		int bet = (int) Math.floor(Math.random() * 10) % 2;
-		System.out.println("Gambler betting value : " +bet);
-		
-		System.out.println("");
-		
-		if (bet == 1)
+		//System.out.println("Bet value is 1 then WON else LOSS");
+
+		for (day = 1; day <= 10; day++)
 		{
-			System.out.println("GAMBLER WON");
-			amount = amount + 1;
-			System.out.println("Gambler new amount after WIN $" +amount);
+			newAmount = newAmount + 100;
+			System.out.println("Gambler amount(+$100) $" +newAmount+ " for day " +day);
+			amount = newAmount;
+			
+			System.out.println("New Amount $" +newAmount);
+			System.out.println("Exit after half of the New Amount $" +amount/2);
+			System.out.println("Exit after double of the New Amount $" +amount*2);
+				
+		
+			while (true)
+			{
+				if (newAmount == amount/2 || newAmount == amount*2)
+				break;
+				
+				int bet = (int) Math.floor(Math.random() * 10) % 2;
+				//System.out.println("Gambler betting value : " +bet);
+				
+				
+				switch(bet)
+				{
+					
+					case 1:
+						//System.out.println("GAMBLER WON");
+						newAmount = newAmount + 1;
+						//System.out.println("Gambler new amount after WIN $" +newAmount);
+					break;
+					
+					default:
+						//System.out.println("GAMBLER LOSS");
+						newAmount = newAmount - 1;
+						//System.out.println("Gambler new amount after LOSS $" +newAmount);
+					break;
+					
+				}
+				
+			
+			}
+			
+			System.out.println();
+			System.out.println("Gambler new amount $" +newAmount+ " after WIN or LOSS for day " +day);
+			System.out.println();
+			
 		}
-		else
-		{
-			System.out.println("GAMBLER LOSS");
-			amount = amount - 1;
-			System.out.println("Gambler new amount after LOSS $" +amount);
-		}
 		
-		
-		
+	
 	}
 }
