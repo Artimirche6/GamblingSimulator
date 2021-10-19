64e0fafda5721e36bc6f13dcca8263d8f95a8f04
Artimirche6 <artimirche6@gmail.com>
2021-10-19 18:59:51 +0530

    [arti]: Gambler Playing for each month

--- Gambler.java
+++ Gambler.java
@@ -1,63 +1,67 @@
 public class Gambler
 {
-	public static void main(String[] args)
+public static void main(String[] args)
 	{	
 		int amount = 100;
 		int betAmount = 1;
-		int day, newAmount  = 0;
-		
+		int day, month, newAmount  = 0;
 		System.out.println("Gambler initial amount $" +amount);
 		System.out.println("Gambler can bet $" +betAmount+ " every game");
 		System.out.println();
 		
 		//System.out.println("Bet value is 1 then WON else LOSS");
-
-		for (day = 1; day <= 20; day++)
+		for (month = 1; month <= 12; month++)
 		{
-			newAmount = newAmount + 100;
-			System.out.println("Gambler amount(+$100) $" +newAmount+ " for day " +day);
-			amount = newAmount;
-			
-			//System.out.println("New Amount $" +newAmount);
-			//System.out.println("Exit after half of the New Amount $" +amount/2);
-			//System.out.println("Exit after double of the New Amount $" +amount*2);
-				
-		
-			while (true)
+			for (day = 1; day <= 20; day++)
 			{
-				if (newAmount == amount/2 || newAmount == amount*2)
-				break;
+				newAmount = newAmount + 100;
+				//System.out.println("Gambler amount(+$100) $" +newAmount+ " for day " +day);
+				amount = newAmount;
 				
-				int bet = (int) Math.floor(Math.random() * 10) % 2;
-				//System.out.println("Gambler betting value : " +bet);
-				
-				
-				switch(bet)
+				//System.out.println("New Amount $" +newAmount);
+				//System.out.println("Exit after half of the New Amount $" +amount/2);
+				//System.out.println("Exit after double of the New Amount $" +amount*2);
+					
+			
+				while (true)
 				{
-					
-					case 1:
-						//System.out.println("GAMBLER WON");
-						newAmount = newAmount + 1;
-						//System.out.println("Gambler new amount after WIN $" +newAmount);
+					if (newAmount == amount/2 || newAmount == amount*2)
 					break;
 					
-					default:
-						//System.out.println("GAMBLER LOSS");
-						newAmount = newAmount - 1;
-						//System.out.println("Gambler new amount after LOSS $" +newAmount);
-					break;
+					int bet = (int) Math.floor(Math.random() * 10) % 2;
+					//System.out.println("Gambler betting value : " +bet);
 					
+					
+					switch(bet)
+					{
+						
+						case 1:
+							//System.out.println("GAMBLER WON");
+							newAmount = newAmount + 1;
+							//System.out.println("Gambler new amount after WIN $" +newAmount);
+						break;
+						
+						default:
+							//System.out.println("GAMBLER LOSS");
+							newAmount = newAmount - 1;
+							//System.out.println("Gambler new amount after LOSS $" +newAmount);
+						break;
+						
+					}
+					
+				
 				}
 				
-			
+				//System.out.println();
+				//System.out.println("Gambler new amount $" +newAmount+ " after WIN or LOSS for day " +day);
+				//System.out.println();
+				
 			}
 			
 			System.out.println();
-			System.out.println("Gambler new amount $" +newAmount+ " after WIN or LOSS for day " +day);
+			System.out.println("Gambler new amount $" +newAmount+ " after WIN or LOSS for month " +month);
 			System.out.println();
-			
 		}
-		
 	
 	}
 }
