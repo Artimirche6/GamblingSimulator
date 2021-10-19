1f51cdacb81eadaf2bd37b7556087bfd88e873cd
Artimirche6 <artimirche6@gmail.com>
2021-10-19 18:44:06 +0530

    [arti]: Gambler Win or Losse the bet

--- Gambler.java
+++ Gambler.java
@@ -2,12 +2,33 @@ public class Gambler
 {
 	public static void main(String[] args)
 	{	
-	
 		int amount = 100;
-		int bet = 1;
+		int betAmount = 1;
 		
 		System.out.println("Gambler initial amount $" +amount);
-		System.out.println("Gambler can bet $" +bet+ " every game");
+		System.out.println("Gambler can bet $" +betAmount+ " every game");
+		System.out.println();
+		System.out.println("Bet value is 1 then WON else LOSS");
+		
+		int bet = (int) Math.floor(Math.random() * 10) % 2;
+		System.out.println("Gambler betting value : " +bet);
+		
+		System.out.println("");
+		
+		if (bet == 1)
+		{
+			System.out.println("GAMBLER WON");
+			amount = amount + 1;
+			System.out.println("Gambler new amount after WIN $" +amount);
+		}
+		else
+		{
+			System.out.println("GAMBLER LOSS");
+			amount = amount - 1;
+			System.out.println("Gambler new amount after LOSS $" +amount);
+		}
+		
+		
 		
 	}
 }
